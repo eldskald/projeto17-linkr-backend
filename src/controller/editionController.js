97cb957728ad595cabd4a16ceaa10b264a292930
Saulo <saulodavi2001@gmail.com>
2022-08-11 14:32:40 -0300

    error connection

--- src/controller/editionController.js
+++ src/controller/editionController.js
@@ -2,15 +2,15 @@ import connection from "../../config/database.js";
 
 export async function editionController (req, res){
     const locals = res.locals
+    console.log('locals :', locals)
     const {id} = req.params
+    console.log('id :', id)
     const email = locals.user.email
     console.log(email)
     try {
-        // const user = connection.query(` SELECT * FROM users
-        //                                 WHERE users.email = '${email}'`)
-        const post = connection.query(`SELECT * FROM users`)
-        console.log('user and post : ', post.rows )
-        return res.status(200).send(post.rows)
+        const users = connection.query(` SELECT * FROM users`)
+        console.log('users:', users.rows )
+        return res.status(200).send(users.rows)
         
     } catch (error) {
         return res.status(500).send('catch editionController')

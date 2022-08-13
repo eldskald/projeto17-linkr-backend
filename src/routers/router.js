a811ff6253af4d512bd567f1e4b4d5d91c044e7e
pink_glb <adriancosta1215@gmail.com>
2022-08-12 23:44:56 -0400

    adding repository and router

--- src/routers/router.js
+++ src/routers/router.js
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import authRouter from './authRouter.js';
 import postsRouter from './postsRouter.js';
 import editionPost from './editionPost.js';
+import hashtagRouter from './hashtagRouter.js';
 
 const router = Router();
 
 router.use(authRouter);
 router.use(postsRouter);
 router.use(editionPost);
+router.use(hashtagRouter);
 
 export default router;
+

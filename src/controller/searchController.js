debfab2c0abca412d55946520892a329075ad9d5
rafael <rafaelbordoni00@gmail.com>
2022-08-16 22:11:06 -0300

    Add priority by following on search bar results

--- src/controller/searchController.js
+++ src/controller/searchController.js
@@ -3,7 +3,8 @@ import { search } from '../repositories/searchRepository.js';
 export async function searchFor(req, res) {
     try {
         const { str, id } = req.query;
-        const results = await search(str);
+        const { userId } = res.locals;
+        const results = await search(str, userId);
         if (results.length > 0) return res.status(200).send({results, id});
         else return res.status(204).send({results, id});
 
